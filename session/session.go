cef288f12f876973aa6ab675a9a371df4cb13885
Cergoo <itcraft.letter@gmail.com>
2014-03-31 14:18:01 +0600

    remove local cache

--- session/session.go
+++ session/session.go
@@ -6,20 +6,17 @@
 package session
 
 import (
-	"gol/cache"
 	"gol/cookie"
 	"gol/err"
 	"gol/genid"
 	"math"
 	"net/http"
-	"time"
 )
 
 type (
 	TSession struct {
-		cache cache.Cache     // local memory cache
-		gen   genid.HTTPGenID // id generator
-		stor  TStor           // store interface implementation
+		gen  genid.HTTPGenID // id generator
+		stor TStor           // store interface implementation
 	}
 
 	TStor interface {
@@ -36,7 +33,6 @@ const (
 // constructor
 func NewSessionEngin(timeLiveInCache, lenID uint8, stor TStor) *TSession {
 	t := new(TSession)
-	t.cache = cache.New(1000, true, time.Minute*time.Duration(timeLiveInCache), nil)
 	t.gen = genid.NewHTTPGen(lenID)
 	t.stor = stor
 	return t
@@ -45,10 +41,7 @@ func NewSessionEngin(timeLiveInCache, lenID uint8, stor TStor) *TSession {
 // Create new session
 func (t *TSession) New(w http.ResponseWriter, data interface{}) (id string) {
 	id = t.gen.NewID()
-	t.cache.Set(id, data)
-	if t.stor != nil {
-		t.stor.Set(id, data)
-	}
+	t.stor.Set(id, data)
 	cookie.SetCookie(w, sid, id, &cookie.Options{Path: "/", MaxAge: math.MaxInt32, HttpOnly: true})
 	return
 }
@@ -57,10 +50,7 @@ func (t *TSession) New(w http.ResponseWriter, data interface{}) (id string) {
 func (t *TSession) Del(w http.ResponseWriter, r *http.Request) {
 	vcoockie, e := r.Cookie(sid)
 	err.Panic(e)
-	t.cache.Del(vcoockie.Value)
-	if t.stor != nil {
-		t.stor.Del(vcoockie.Value)
-	}
+	t.stor.Del(vcoockie.Value)
 	cookie.DelCookie(w, sid)
 }
 
@@ -71,20 +61,10 @@ func (t *TSession) Get(w http.ResponseWriter, r *http.Request) interface{} {
 	if e != nil {
 		return nil
 	}
-
-	val = t.cache.Get(vcoockie.Value)
-	// if cache miss
+	val = t.stor.Get(vcoockie.Value)
+	// if session delet then del cookie
 	if val == nil {
-		if t.stor != nil {
-			val = t.stor.Get(vcoockie.Value)
-		}
-		// if session delet then del cookie
-		if val == nil {
-			cookie.DelCookie(w, sid)
-			return nil
-		} else {
-			t.cache.Set(vcoockie.Value, val)
-		}
+		cookie.DelCookie(w, sid)
 	}
 	return val
 }

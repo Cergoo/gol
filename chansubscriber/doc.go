dcac105c172e2bcffc9cfac95bc08648429f0ba8
Cergoo <itcraft.letter@gmail.com>
2014-08-26 11:16:36 +0600

    mod

--- chansubscriber/doc.go
+++ chansubscriber/doc.go
@@ -1,8 +1,9 @@
-// subcribe channel pack, send messages of writer to a each subscribers
 // (c) 2014 Cergoo
 // under terms of ISC license
 	
 /*
+Subcribe channel pack, send messages of writer to a each subscribers.
+
 Features:  
   - thread safe;
   - protect of a double subscribe;

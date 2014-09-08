7e146b31a1dc0cdfee2990e2dfe0630e2c5fc0fd
Cergoo <itcraft.letter@gmail.com>
2014-09-08 19:55:36 +0600

    mod

--- binaryED/binaryED/doc.go
+++ binaryED/binaryED/doc.go
@@ -5,8 +5,8 @@
 Package binaryED it's a binary structure less format encode/decode implementation
 fork github.com/youtube/vitess/go/bson
 
-Attention!!!    
-======================================================================
+Attention!        
+======================================================================        
 Before you can use this package need to patch standard library reflect,
 for unto this add the file the following function:
 go/src/pkg/reflect/value.go
@@ -27,22 +27,20 @@ The package is designed for fast serialization / deserialization:
 	struct
 
 Important !!!    
-
 ======================================================================
-
-- Nonexported field structures are ignored.
+- Nonexported field structures are ignored.    
 - In decoding the variable structure is used in which the decoding occurs,
 necessary to match the structure of the receiver structure of the source
-up to the order of the fields in the description of the structures.
+up to the order of the fields in the description of the structures.    
 - Possible encoding / decoding only a strictly structured data,
-ie map[string]interfase {} can not be coded as values ​​map do not have a strict structure.
+ie map[string]interfase {} can not be coded as values ​​map do not have a strict structure.    
 
 Важно!!!
     
-- При кодировании/декодировании неэкспортируемые поля структур игнорируются.
-- При декодировании используется структура переменной в которую происходит декодирование, необходимо чтобы структура приёмнника соответсвовала структуре источника вплоть до порядка следования полей в описании структур.
+- При кодировании/декодировании неэкспортируемые поля структур игнорируются.     
+- При декодировании используется структура переменной в которую происходит декодирование, необходимо чтобы структура приёмнника соответсвовала структуре источника вплоть до порядка следования полей в описании структур.    
 - Возможно кодирование/декодирование только строго структурированных данных,
 то есть map[string]interfase{} нельзя кодировать так как значения хештаблицы
-не имеют описания структуры а формат кодирования некодирует описание структур а только их данные.
+не имеют описания структуры а формат кодирования некодирует описание структур а только их данные.     
 */
 package binaryED

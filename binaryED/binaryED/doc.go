dfc594008ebed97d6498ec3cd604c00e9bbafb0f
Cergoo <itcraft.letter@gmail.com>
2014-09-08 20:04:13 +0600

    mod

--- binaryED/binaryED/doc.go
+++ binaryED/binaryED/doc.go
@@ -8,6 +8,7 @@ fork github.com/youtube/vitess/go/bson
 Attention! 
        
 ======================================================================        
+
 Before you can use this package need to patch standard library reflect,
 for unto this add the file the following function:
 go/src/pkg/reflect/value.go
@@ -30,17 +31,22 @@ The package is designed for fast serialization / deserialization:
 Important !  
        
 ======================================================================
+
 - Nonexported field structures are ignored.    
+
 - In decoding the variable structure is used in which the decoding occurs,
 necessary to match the structure of the receiver structure of the source
 up to the order of the fields in the description of the structures.    
+
 - Possible encoding / decoding only a strictly structured data,
 ie map[string]interfase {} can not be coded as values ​​map do not have a strict structure.    
 
 Важно!    
     
 - При кодировании/декодировании неэкспортируемые поля структур игнорируются.     
+
 - При декодировании используется структура переменной в которую происходит декодирование, необходимо чтобы структура приёмнника соответсвовала структуре источника вплоть до порядка следования полей в описании структур.    
+
 - Возможно кодирование/декодирование только строго структурированных данных,
 то есть map[string]interfase{} нельзя кодировать так как значения хештаблицы
 не имеют описания структуры а формат кодирования некодирует описание структур а только их данные.     
